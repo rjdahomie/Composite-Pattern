1035e29eee29a0972c4214a6a8063936d1f2c262
rjdahomie <rasajahromi@gmail.com>
2022-05-09 15:15:45 -0700

    Rand Class
    Added the rand class

--- op.hpp
+++ op.hpp
@@ -1,13 +1,43 @@
 #ifndef __OP_HPP__
 #define __OP_HPP__
 
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
 #include "base.hpp"
+
+using namespace std;
 
 class Op : public Base {
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base() { number = value; }
+        virtual double evaluate() { return number; }
+        virtual std::string stringify() { 
+		stringstream num;
+		num << number;
+		string s;
+		num << s;
+		return s;
+	 }
+	private:
+	    double number;
 };
+
+class Rand : public Base {
+    public:
+        Op(double value) : Base() { number = rand() % 100; }
+        virtual double evaluate() { return number; }
+        virtual std::string stringify() {
+                stringstream num;
+                num << number;
+                string s;
+                num << s;
+                return s;
+         }
+        private:
+            double number;
+};
+
+
 
 #endif //__OP_HPP__
