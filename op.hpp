7d7fde79f9fd9bc3cb88b21839e2fd068f66a770
thecornerhob <94258836+thecornerhob@users.noreply.github.com>
2022-05-10 14:13:06 -0700

    Update op.hpp
    Updated your file, switched Rand class so itll make a rand object rather than an Op object. Added Nodes to each class so they can be utilized by pointers. Updated stringify to a simpler style

--- op.hpp
+++ op.hpp
@@ -1,9 +1,7 @@
 #ifndef __OP_HPP__
 #define __OP_HPP__
 
-#include <iostream>
-#include <sstream>
-#include <cstdlib>
+#include <string>
 #include "base.hpp"
 
 using namespace std;
@@ -12,30 +10,22 @@ class Op : public Base {
     public:
         Op(double value) : Base() { number = value; }
         virtual double evaluate() { return number; }
-        virtual std::string stringify() { 
-		stringstream num;
-		num << number;
-		string s;
-		num << s;
-		return s;
-	 }
-	private:
-	    double number;
+        virtual std::string stringify() { return std::to_string(myVal); }
+    private:
+	Base* leftNode = NULL;
+	Base* rightNode = NULL;
+	double number;
 };
 
 class Rand : public Base {
     public:
-        Op(double value) : Base() { number = rand() % 100; }
+        Rand(double value) : Base() { number = rand() % 100; }
         virtual double evaluate() { return number; }
-        virtual std::string stringify() {
-                stringstream num;
-                num << number;
-                string s;
-                num << s;
-                return s;
-         }
-        private:
-            double number;
+        virtual std::string stringify() { return std::to_string(number); }
+    private:
+	Base* leftNode = NULL;
+	Base* rightNode = NULL;
+	double number;
 };
 
 
