673777a9718419bec9d8acb463a774c54720441e
rjdahomie <rasajahromi@gmail.com>
2022-05-10 15:35:33 -0700

    Added the sub and rand classes
    *Sub and Rand classes
    *Sub and Rand unit tests

---
+++ rand.hpp
@@ -0,0 +1,22 @@
+#ifndef __RAND_HPP__
+#define __RAND_HPP__
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "base.hpp"
+
+class Rand : public Base {
+    public:
+        Rand() : Base() { number = rand() % 100; }
+        virtual double evaluate() { return number; }
+        virtual std::string stringify() {return to_string(number);}
+        double getValue(){return number;}
+        void setVal (double v){number = v;}
+    private:
+        double number = 0.0;
+        Base* leftNode = NULL;
+        Base* rightNode = NULL;
+};
+
+#endif
--- sub_test.hpp
+++ sub_test.hpp
@@ -38,6 +38,13 @@ TEST(SubTest, EvaluateZero){
     delete a,b,test;
 }
 
+TEST(SubTest, Stringify_One){
+    Op* a = new Op(10.0);
+    Op* b = new Op(5.0);
+    Sub* test = new Sub(a,b);
+    EXPECT_EQ("(10.000000-5.000000)", test->stringify());
+    delete a,b,test;
+}
 
 
 #endif //SUB_TEST_HPP
